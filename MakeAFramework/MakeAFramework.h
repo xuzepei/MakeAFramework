78c924b789331d5f661c2b81e37239a1d7f9b7b9
xuzepei <xuzepei@smalltreemedia.com>
2015-10-15 14:11:02 +0800

    Initial Commit

---
+++ MakeAFramework/MakeAFramework.h
@@ -0,0 +1,19 @@
+//
+//  MakeAFramework.h
+//  MakeAFramework
+//
+//  Created by xuzepei on 15/10/15.
+//  Copyright (c) 2015年 moki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MakeAFramework.
+FOUNDATION_EXPORT double MakeAFrameworkVersionNumber;
+
+//! Project version string for MakeAFramework.
+FOUNDATION_EXPORT const unsigned char MakeAFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MakeAFramework/PublicHeader.h>
+
+
